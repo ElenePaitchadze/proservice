cf05a55bfa8293e5c784bf6224180250fbb5708d
e.paitchadze <e.paitchadze@proservice.ge>
2025-11-14 22:32:40 +0400

    new changed to header menu

--- components/ProductMenu/ServiceMenu.tsx
+++ components/ProductMenu/ServiceMenu.tsx
@@ -4,6 +4,8 @@ import styles from './productMenu.module.css';
 import topIcon from '@/img/topicons.png';
 import bottomIcon from '@/img/bottomicon.png';
 import firstMenuImg from'@/img/firstMenuImg.png';
+import secondMenuImg from'@/img/secondMenuImg.png';
+import thirdMenuImg from'@/img/thirdMenuImg.png';
 
 export default function ServiceMenu({ data }: { data: IProdMenu }) {
   return (
@@ -14,6 +16,21 @@ export default function ServiceMenu({ data }: { data: IProdMenu }) {
         <img src={firstMenuImg.src} alt="image" />
         <img src={bottomIcon.src} alt="icon" />
       </div>
+      <div id={styles.service2}>
+        <p>IT მომსახურება</p>
+        <span>B2B კომპიუტერული მომსახურების სერვისი</span>
+        <div>
+          <img src={secondMenuImg.src} alt="image" />
+          <span>ITSERVICE.GE</span>
+        </div>
+      </div>
+      <div id={styles.service3}>
+        <p>საიტის შემქმნელი</p>
+        <img src={thirdMenuImg.src} alt="image" />
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
